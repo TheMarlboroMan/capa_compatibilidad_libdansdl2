639be919cfbe53a6b643e9b8fb5a441e6a1e33cf
Daniel Pastor <daniel.pastor@grupoancana.com>
2015-11-05 10:59:39 +0100

    Actualizaciones.

--- capa_compatibilidad_representaciones_libdansdl2.h
+++ capa_compatibilidad_representaciones_libdansdl2.h
@@ -40,9 +40,11 @@ class Representacion_bitmap_estatica
 	:public Representacion_bitmap
 {
 	public:
+			Representacion_bitmap_estatica()
+				:Representacion_bitmap() {hacer_estatica();}
 			Representacion_bitmap_estatica(const Representacion_bitmap& v)
 				:Representacion_bitmap(v) {hacer_estatica();}
-			Representacion_bitmap_estatica(const Textura * v):
+			Representacion_bitmap_estatica(const Textura * v)
 				:Representacion_bitmap(v) {hacer_estatica();}
 	Representacion_bitmap_estatica& operator=(const Representacion_bitmap_estatica & v)
 	{
@@ -57,9 +59,11 @@ class Representacion_bitmap_dinamica
 	:public Representacion_bitmap
 {
 	public:
+			Representacion_bitmap_dinamica()
+				:Representacion_bitmap() {hacer_dinamica();}
 			Representacion_bitmap_dinamica(const Representacion_bitmap& v)
 				:Representacion_bitmap(v) {hacer_dinamica();}
-			Representacion_bitmap_dinamica(const Textura * v):
+			Representacion_bitmap_dinamica(const Textura * v)
 				:Representacion_bitmap(v) {hacer_dinamica();}
 	Representacion_bitmap_dinamica& operator=(const Representacion_bitmap_dinamica & v)
 	{
@@ -83,7 +87,7 @@ class Representacion_bitmap_patron_estatica
 				:Representacion_bitmap_patron(v) {hacer_estatica();}
 			Representacion_bitmap_patron_estatica(const Textura * v)
 				:Representacion_bitmap_patron(v) {hacer_estatica();}
-			Representacion_bitmap_patron_estatica& operator=(const Representacion_bitmap_patron & o)
+			Representacion_bitmap_patron_estatica& operator=(const Representacion_bitmap_patron & v)
 			{
 				Representacion_bitmap_patron::operator=(v);
 				hacer_estatica();
@@ -103,7 +107,7 @@ class Representacion_bitmap_patron_dinamica
 				:Representacion_bitmap_patron(v) {hacer_dinamica();}
 			Representacion_bitmap_patron_dinamica(const Textura * v)
 				:Representacion_bitmap_patron(v) {hacer_dinamica();}
-			Representacion_bitmap_patron_dinamica& operator=(const Representacion_bitmap_patron & o)
+			Representacion_bitmap_patron_dinamica& operator=(const Representacion_bitmap_patron & v)
 			{
 				Representacion_bitmap_patron::operator=(v);
 				hacer_dinamica();
@@ -121,11 +125,11 @@ class Representacion_texto_auto_estatica
 
 			Representacion_texto_auto_estatica(const DLibV::Superficie * p_rep, const char p_char)
 				:Representacion_texto_auto(p_rep, p_char) {hacer_estatica();}
-			Representacion_texto_auto_estatica(const DLibV::Superficie * p_rep, const char * p_char):
+			Representacion_texto_auto_estatica(const DLibV::Superficie * p_rep, const char * p_char)
 				:Representacion_texto_auto(p_rep, p_char) {hacer_estatica();}
-			Representacion_texto_auto_estatica(const DLibV::Superficie * p_rep, std::string const& p_cad);
+			Representacion_texto_auto_estatica(const DLibV::Superficie * p_rep, std::string const& p_cad)
 				:Representacion_texto_auto(p_rep, p_cad) {hacer_estatica();}
-			Representacion_texto_auto_estatica(const Representacion_texto_auto_estatica& v);
+			Representacion_texto_auto_estatica(const Representacion_texto_auto_estatica& v)
 				:Representacion_texto_auto(v) {hacer_estatica();}
 			Representacion_texto_auto_estatica& operator=(const Representacion_texto_auto_estatica& v)
 			{
@@ -143,11 +147,11 @@ class Representacion_texto_auto_dinamica
 
 			Representacion_texto_auto_dinamica(const DLibV::Superficie * p_rep, const char p_char)
 				:Representacion_texto_auto(p_rep, p_char) {hacer_dinamica();}
-			Representacion_texto_auto_dinamica(const DLibV::Superficie * p_rep, const char * p_char):
+			Representacion_texto_auto_dinamica(const DLibV::Superficie * p_rep, const char * p_char)
 				:Representacion_texto_auto(p_rep, p_char) {hacer_dinamica();}
-			Representacion_texto_auto_dinamica(const DLibV::Superficie * p_rep, std::string const& p_cad);
+			Representacion_texto_auto_dinamica(const DLibV::Superficie * p_rep, std::string const& p_cad)
 				:Representacion_texto_auto(p_rep, p_cad) {hacer_dinamica();}
-			Representacion_texto_auto_dinamica(const Representacion_texto_auto_dinamica& v);
+			Representacion_texto_auto_dinamica(const Representacion_texto_auto_dinamica& v)
 				:Representacion_texto_auto(v) {hacer_dinamica();}
 			Representacion_texto_auto_dinamica& operator=(const Representacion_texto_auto_dinamica& v)
 			{
@@ -164,14 +168,14 @@ class Representacion_texto_fija_estatica
 	:public Representacion_texto_fija
 {
 	public:
-			Representacion_texto_fija_estatica(const DLibV::Superficie * p_rep, const char p_char, unsigned int p_ancho, unsigned int p_alto):
-				Representacion_texto_fija(p_rep, p_char, p_ancho, p_alto) {hacer_estatica();}
-			Representacion_texto_fija_estatica(const DLibV::Superficie * p_rep, const char * p_char, unsigned int p_ancho, unsigned int p_alto);
-				Representacion_texto_fija(p_rep, p_char, p_ancho, p_alto) {hacer_estatica();}
-			Representacion_texto_fija_estatica(const DLibV::Superficie * p_rep, std::string const& p_cad,unsigned int p_ancho, unsigned int p_alto);
-				Representacion_texto_fija(p_rep, p_cad, p_ancho, p_alto) {hacer_estatica();}
-			Representacion_texto_fija_estatica(const Representacion_texto_fija_estatica& v):
-				Representacion_texto_fija(v) {hacer_estatica();}
+			Representacion_texto_fija_estatica(const DLibV::Superficie * p_rep, const char p_char, unsigned int p_ancho, unsigned int p_alto)
+				:Representacion_texto_fija(p_rep, p_char, p_ancho, p_alto) {hacer_estatica();}
+			Representacion_texto_fija_estatica(const DLibV::Superficie * p_rep, const char * p_char, unsigned int p_ancho, unsigned int p_alto)
+				:Representacion_texto_fija(p_rep, p_char, p_ancho, p_alto) {hacer_estatica();}
+			Representacion_texto_fija_estatica(const DLibV::Superficie * p_rep, std::string const& p_cad,unsigned int p_ancho, unsigned int p_alto)
+				:Representacion_texto_fija(p_rep, p_cad, p_ancho, p_alto) {hacer_estatica();}
+			Representacion_texto_fija_estatica(const Representacion_texto_fija_estatica& v)
+				:Representacion_texto_fija(v) {hacer_estatica();}
 			Representacion_texto_fija_estatica& operator=(const Representacion_texto_fija_estatica& v)
 			{
 				Representacion_texto_fija::operator=(v);
@@ -185,14 +189,14 @@ class Representacion_texto_fija_dinamica
 	:public Representacion_texto_fija
 {
 	public:
-			Representacion_texto_fija_dinamica(const DLibV::Superficie * p_rep, const char p_char, unsigned int p_ancho, unsigned int p_alto):
-				Representacion_texto_fija(p_rep, p_char, p_ancho, p_alto) {hacer_dinamica();}
-			Representacion_texto_fija_dinamica(const DLibV::Superficie * p_rep, const char * p_char, unsigned int p_ancho, unsigned int p_alto);
-				Representacion_texto_fija(p_rep, p_char, p_ancho, p_alto) {hacer_dinamica();}
-			Representacion_texto_fija_dinamica(const DLibV::Superficie * p_rep, std::string const& p_cad,unsigned int p_ancho, unsigned int p_alto);
-				Representacion_texto_fija(p_rep, p_cad, p_ancho, p_alto) {hacer_dinamica();}
-			Representacion_texto_fija_dinamica(const Representacion_texto_fija_dinamica& v):
-				Representacion_texto_fija(v) {hacer_dinamica();}
+			Representacion_texto_fija_dinamica(const DLibV::Superficie * p_rep, const char p_char, unsigned int p_ancho, unsigned int p_alto)
+				:Representacion_texto_fija(p_rep, p_char, p_ancho, p_alto) {hacer_dinamica();}
+			Representacion_texto_fija_dinamica(const DLibV::Superficie * p_rep, const char * p_char, unsigned int p_ancho, unsigned int p_alto)
+				:Representacion_texto_fija(p_rep, p_char, p_ancho, p_alto) {hacer_dinamica();}
+			Representacion_texto_fija_dinamica(const DLibV::Superficie * p_rep, std::string const& p_cad,unsigned int p_ancho, unsigned int p_alto)
+				:Representacion_texto_fija(p_rep, p_cad, p_ancho, p_alto) {hacer_dinamica();}
+			Representacion_texto_fija_dinamica(const Representacion_texto_fija_dinamica& v)
+				:Representacion_texto_fija(v) {hacer_dinamica();}
 			Representacion_texto_fija_dinamica& operator=(const Representacion_texto_fija_dinamica& v)
 			{
 				Representacion_texto_fija::operator=(v);
@@ -204,21 +208,21 @@ class Representacion_texto_fija_dinamica
 
 /** Representacion TTF */
 
-class Representacion_TTF_estatica:
-	public Representacion_TTF
+class Representacion_TTF_estatica
+	:public Representacion_TTF
 {
 	public:
 
-			Representacion_TTF_estatica(const Fuente_TTF& a, SDL_Color b, const std::string& c):
-				Representacion_TTF(a, b, c) {hacer_estatica();}
-			Representacion_TTF_estatica(const Fuente_TTF& f, Uint8 r, Uint8 g, Uint8 b, Uint8 a, const std::string& c);
-				Representacion_TTF(f, r, g, b, a, c) {hacer_estatica();}
-			Representacion_TTF_estatica(const Fuente_TTF& a, SDL_Color b):
-				Representacion_TTF(a, b) {hacer_estatica();}
-			Representacion_TTF_estatica(const Fuente_TTF& f, Uint8 r, Uint8 g, Uint8 b, Uint8 a):
-				Representacion_TTF(f, r, g, b, a) {hacer_estatica();}
-			Representacion_TTF_estatica(const Representacion_TTF_estatica& v):
-				Representacion_TTF(v) {hacer_estatica();}
+			Representacion_TTF_estatica(const Fuente_TTF& a, SDL_Color b, const std::string& c)
+				:Representacion_TTF(a, b, c) {hacer_estatica();}
+			Representacion_TTF_estatica(const Fuente_TTF& f, Uint8 r, Uint8 g, Uint8 b, Uint8 a, const std::string& c)
+				:Representacion_TTF(f, r, g, b, a, c) {hacer_estatica();}
+			Representacion_TTF_estatica(const Fuente_TTF& a, SDL_Color b)
+				:Representacion_TTF(a, b) {hacer_estatica();}
+			Representacion_TTF_estatica(const Fuente_TTF& f, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+				:Representacion_TTF(f, r, g, b, a) {hacer_estatica();}
+			Representacion_TTF_estatica(const Representacion_TTF_estatica& v)
+				:Representacion_TTF(v) {hacer_estatica();}
 			Representacion_TTF_estatica& operator=(const Representacion_TTF_estatica& v)
 			{
 				Representacion_TTF::operator=(v);
@@ -233,16 +237,16 @@ class Representacion_TTF_dinamica:
 {
 	public:
 
-			Representacion_TTF_dinamica(const Fuente_TTF& a, SDL_Color b, const std::string& c):
-				Representacion_TTF(a, b, c) {hacer_dinamica();}
-			Representacion_TTF_dinamica(const Fuente_TTF& f, Uint8 r, Uint8 g, Uint8 b, Uint8 a, const std::string& c);
-				Representacion_TTF(f, r, g, b, a, c) {hacer_dinamica();}
-			Representacion_TTF_dinamica(const Fuente_TTF& a, SDL_Color b):
-				Representacion_TTF(a, b) {hacer_dinamica();}
-			Representacion_TTF_dinamica(const Fuente_TTF& f, Uint8 r, Uint8 g, Uint8 b, Uint8 a):
-				Representacion_TTF(f, r, g, b, a) {hacer_dinamica();}
-			Representacion_TTF_dinamica(const Representacion_TTF_dinamica& v):
-				Representacion_TTF(v) {hacer_dinamica();}
+			Representacion_TTF_dinamica(const Fuente_TTF& a, SDL_Color b, const std::string& c)
+				:Representacion_TTF(a, b, c) {hacer_dinamica();}
+			Representacion_TTF_dinamica(const Fuente_TTF& f, Uint8 r, Uint8 g, Uint8 b, Uint8 a, const std::string& c)
+				:Representacion_TTF(f, r, g, b, a, c) {hacer_dinamica();}
+			Representacion_TTF_dinamica(const Fuente_TTF& a, SDL_Color b)
+				:Representacion_TTF(a, b) {hacer_dinamica();}
+			Representacion_TTF_dinamica(const Fuente_TTF& f, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+				:Representacion_TTF(f, r, g, b, a) {hacer_dinamica();}
+			Representacion_TTF_dinamica(const Representacion_TTF_dinamica& v)
+				:Representacion_TTF(v) {hacer_dinamica();}
 			Representacion_TTF_dinamica& operator=(const Representacion_TTF_dinamica& v)
 			{
 				Representacion_TTF::operator=(v);
@@ -260,9 +264,9 @@ class Representacion_primitiva_caja_estatica
 	public:
 	
 				Representacion_primitiva_caja_estatica(const SDL_Rect& p_pos, Uint8 pr, Uint8 pg, Uint8 pb)
-						:Representacion_primitiva_caja(p_pos, pr, pg, pb) {hacer_estatica();}
+					:Representacion_primitiva_caja(p_pos, pr, pg, pb) {hacer_estatica();}
 				Representacion_primitiva_caja_estatica(const Representacion_primitiva_caja_estatica& v)
-						:Representacion_primitiva_caja(v) {hacer_estatica();}
+					:Representacion_primitiva_caja(v) {hacer_estatica();}
 				Representacion_primitiva_caja_estatica& operator=(const Representacion_primitiva_caja_estatica& v)
 				{
 					Representacion_primitiva_caja::operator=(v);
@@ -278,9 +282,9 @@ class Representacion_primitiva_caja_dinamica
 	public:
 	
 				Representacion_primitiva_caja_dinamica(const SDL_Rect& p_pos, Uint8 pr, Uint8 pg, Uint8 pb)
-						:Representacion_primitiva_caja(p_pos, pr, pg, pb) {hacer_dinamica();}
+					:Representacion_primitiva_caja(p_pos, pr, pg, pb) {hacer_dinamica();}
 				Representacion_primitiva_caja_dinamica(const Representacion_primitiva_caja_dinamica& v)
-						:Representacion_primitiva_caja(v) {hacer_dinamica();}
+					:Representacion_primitiva_caja(v) {hacer_dinamica();}
 				Representacion_primitiva_caja_dinamica& operator=(const Representacion_primitiva_caja_dinamica& v)
 				{
 					Representacion_primitiva_caja::operator=(v);
@@ -300,7 +304,7 @@ class Representacion_primitiva_caja_lineas_estatica
 				Representacion_primitiva_caja_lineas_estatica(const SDL_Rect& p_pos, Uint8 pr, Uint8 pg, Uint8 pb)
 					:Representacion_primitiva_caja_lineas(p_pos, pr, pg, pb){hacer_estatica();}
 				Representacion_primitiva_caja_lineas_estatica(const Representacion_primitiva_caja_lineas_estatica& v)
-					:Representacion_primitiva_caja_lineas(v) {hacer_estatica()}
+					:Representacion_primitiva_caja_lineas(v) {hacer_estatica();}
 				Representacion_primitiva_caja_lineas_estatica& operator=(const Representacion_primitiva_caja_lineas_estatica& v)
 				{
 					Representacion_primitiva_caja_lineas::operator=(v);
@@ -318,7 +322,7 @@ class Representacion_primitiva_caja_lineas_dinamica
 				Representacion_primitiva_caja_lineas_dinamica(const SDL_Rect& p_pos, Uint8 pr, Uint8 pg, Uint8 pb)
 					:Representacion_primitiva_caja_lineas(p_pos, pr, pg, pb){hacer_dinamica();}
 				Representacion_primitiva_caja_lineas_dinamica(const Representacion_primitiva_caja_lineas_dinamica& v)
-					:Representacion_primitiva_caja_lineas(v) {hacer_dinamica()}
+					:Representacion_primitiva_caja_lineas(v) {hacer_dinamica();}
 				Representacion_primitiva_caja_lineas_dinamica& operator=(const Representacion_primitiva_caja_lineas_dinamica& v)
 				{
 					Representacion_primitiva_caja_lineas::operator=(v);
@@ -375,7 +379,7 @@ class Representacion_primitiva_puntos_estatica
 	
 				Representacion_primitiva_puntos_estatica(Uint8 r, Uint8 g, Uint8 b)
 					:Representacion_primitiva_puntos(r, g, b) {hacer_estatica();}
-				Representacion_primitiva_puntos_estatica(int x, int y, Uint8 r, Uint8 g, Uint8 b);
+				Representacion_primitiva_puntos_estatica(int x, int y, Uint8 r, Uint8 g, Uint8 b)
 					:Representacion_primitiva_puntos(x, y, r, g, b) {hacer_estatica();}
 				Representacion_primitiva_puntos_estatica(const Representacion_primitiva_puntos_estatica& v)
 					:Representacion_primitiva_puntos(v) {hacer_estatica();}
@@ -395,7 +399,7 @@ class Representacion_primitiva_puntos_dinamica
 	
 				Representacion_primitiva_puntos_dinamica(Uint8 r, Uint8 g, Uint8 b)
 					:Representacion_primitiva_puntos(r, g, b) {hacer_dinamica();}
-				Representacion_primitiva_puntos_dinamica(int x, int y, Uint8 r, Uint8 g, Uint8 b);
+				Representacion_primitiva_puntos_dinamica(int x, int y, Uint8 r, Uint8 g, Uint8 b)
 					:Representacion_primitiva_puntos(x, y, r, g, b) {hacer_dinamica();}
 				Representacion_primitiva_puntos_dinamica(const Representacion_primitiva_puntos_dinamica& v)
 					:Representacion_primitiva_puntos(v) {hacer_dinamica();}
